e860450a269a939cfe5b57e8550561db0f93acb6
dzidon <david.zidon2@gmail.com>
2022-04-22 18:11:17 +0200

    upraven soubor .platform.app.yaml; v NavbarService se bude získávat aktuální request jen když je to potřeba

--- src/Service/NavbarService.php
+++ src/Service/NavbarService.php
@@ -17,11 +17,11 @@ use Symfony\Component\Security\Core\Security;
  */
 class NavbarService
 {
-    private $currentRoute;
     private array $sections = [];
     private bool $sectionsLoaded = false;
 
     private Security $security;
+    private RequestStack $requestStack;
     private UrlGeneratorInterface $router;
     private ParameterBagInterface $parameterBag;
     private EntityManagerInterface $entityManager;
@@ -32,7 +32,7 @@ class NavbarService
         $this->security = $security;
         $this->parameterBag = $parameterBag;
         $this->entityManager = $entityManager;
-        $this->currentRoute = $requestStack->getCurrentRequest()->attributes->get('_route');
+        $this->requestStack = $requestStack;
     }
 
     /**
@@ -61,26 +61,28 @@ class NavbarService
      */
     public function getProfileNavigationData(): array
     {
+        $currentRoute = $this->requestStack->getCurrentRequest()->attributes->get('_route');
+
         return
         [
             [
                 'path'      => $this->router->generate('profile'),
-                'isActive'  => $this->currentRoute === 'profile',
+                'isActive'  => $currentRoute === 'profile',
                 'title'     => $this->parameterBag->get('app_page_title.profile')
             ],
             [
                 'path'      => $this->router->generate('profile_orders'),
-                'isActive'  => $this->currentRoute === 'profile_orders' || $this->currentRoute === 'profile_order',
+                'isActive'  => $currentRoute === 'profile_orders' || $currentRoute === 'profile_order',
                 'title'     => $this->parameterBag->get('app_page_title.profile_orders')
             ],
             [
                 'path'      => $this->router->generate('profile_addresses'),
-                'isActive'  => $this->currentRoute === 'profile_addresses' || $this->currentRoute === 'profile_address' || $this->currentRoute === 'profile_address_delete',
+                'isActive'  => $currentRoute === 'profile_addresses' || $currentRoute === 'profile_address' || $currentRoute === 'profile_address_delete',
                 'title'     => $this->parameterBag->get('app_page_title.profile_addresses')
             ],
             [
                 'path'      => $this->router->generate('profile_change_password'),
-                'isActive'  => $this->currentRoute === 'profile_change_password',
+                'isActive'  => $currentRoute === 'profile_change_password',
                 'title'     => $this->parameterBag->get('app_page_title.profile_change_password')
             ],
         ];
@@ -93,66 +95,68 @@ class NavbarService
      */
     public function getAdminNavigationData(): array
     {
+        $currentRoute = $this->requestStack->getCurrentRequest()->attributes->get('_route');
+
         return
         [
             [
                 'granted'   => $this->security->isGranted('admin_permission_overview'),
                 'path'      => $this->router->generate('admin_permission_overview'),
-                'isActive'  => $this->currentRoute === 'admin_permission_overview',
+                'isActive'  => $currentRoute === 'admin_permission_overview',
                 'title'     => $this->parameterBag->get('app_page_title.admin_permission_overview')
             ],
             [
                 'granted'   => $this->security->isGranted('admin_user_management'),
                 'path'      => $this->router->generate('admin_user_management'),
-                'isActive'  => $this->currentRoute === 'admin_user_management' || $this->currentRoute === 'admin_user_management_specific',
+                'isActive'  => $currentRoute === 'admin_user_management' || $currentRoute === 'admin_user_management_specific',
                 'title'     => $this->parameterBag->get('app_page_title.admin_user_management')
             ],
             [
                 'granted'   => $this->security->isGranted('admin_orders'),
                 'path'      => $this->router->generate('admin_orders'),
-                'isActive'  => $this->currentRoute === 'admin_orders' || $this->currentRoute === 'admin_order_overview' || $this->currentRoute === 'admin_order_cancel' || $this->currentRoute === 'admin_order_custom_delete' || $this->currentRoute === 'admin_order_custom_edit',
+                'isActive'  => $currentRoute === 'admin_orders' || $currentRoute === 'admin_order_overview' || $currentRoute === 'admin_order_cancel' || $currentRoute === 'admin_order_custom_delete' || $currentRoute === 'admin_order_custom_edit',
                 'title'     => $this->parameterBag->get('app_page_title.admin_orders')
             ],
             [
                 'granted'   => $this->security->isGranted('admin_products'),
                 'path'      => $this->router->generate('admin_products'),
-                'isActive'  => $this->currentRoute === 'admin_products' || $this->currentRoute === 'admin_product_edit' || $this->currentRoute === 'admin_product_delete',
+                'isActive'  => $currentRoute === 'admin_products' || $currentRoute === 'admin_product_edit' || $currentRoute === 'admin_product_delete',
                 'title'     => $this->parameterBag->get('app_page_title.admin_products')
             ],
             [
                 'granted'   => $this->security->isGranted('admin_product_sections'),
                 'path'      => $this->router->generate('admin_product_sections'),
-                'isActive'  => $this->currentRoute === 'admin_product_sections' || $this->currentRoute === 'admin_product_section_edit' || $this->currentRoute === 'admin_product_section_delete',
+                'isActive'  => $currentRoute === 'admin_product_sections' || $currentRoute === 'admin_product_section_edit' || $currentRoute === 'admin_product_section_delete',
                 'title'     => $this->parameterBag->get('app_page_title.admin_product_sections')
             ],
             [
                 'granted'   => $this->security->isGranted('admin_product_categories'),
                 'path'      => $this->router->generate('admin_product_categories'),
-                'isActive'  => $this->currentRoute === 'admin_product_categories' || $this->currentRoute === 'admin_product_category_edit' || $this->currentRoute === 'admin_product_category_delete',
+                'isActive'  => $currentRoute === 'admin_product_categories' || $currentRoute === 'admin_product_category_edit' || $currentRoute === 'admin_product_category_delete',
                 'title'     => $this->parameterBag->get('app_page_title.admin_product_categories')
             ],
             [
                 'granted'   => $this->security->isGranted('admin_product_options'),
                 'path'      => $this->router->generate('admin_product_options'),
-                'isActive'  => $this->currentRoute === 'admin_product_options' || $this->currentRoute === 'admin_product_option_edit' || $this->currentRoute === 'admin_product_option_delete',
+                'isActive'  => $currentRoute === 'admin_product_options' || $currentRoute === 'admin_product_option_edit' || $currentRoute === 'admin_product_option_delete',
                 'title'     => $this->parameterBag->get('app_page_title.admin_product_options')
             ],
             [
                 'granted'   => $this->security->isGranted('admin_product_info'),
                 'path'      => $this->router->generate('admin_product_info'),
-                'isActive'  => $this->currentRoute === 'admin_product_info' || $this->currentRoute === 'admin_product_info_edit' || $this->currentRoute === 'admin_product_info_delete',
+                'isActive'  => $currentRoute === 'admin_product_info' || $currentRoute === 'admin_product_info_edit' || $currentRoute === 'admin_product_info_delete',
                 'title'     => $this->parameterBag->get('app_page_title.admin_product_info')
             ],
             [
                 'granted'   => $this->security->isGranted('admin_delivery_methods'),
                 'path'      => $this->router->generate('admin_delivery_methods'),
-                'isActive'  => $this->currentRoute === 'admin_delivery_methods' || $this->currentRoute === 'admin_delivery_method_edit',
+                'isActive'  => $currentRoute === 'admin_delivery_methods' || $currentRoute === 'admin_delivery_method_edit',
                 'title'     => $this->parameterBag->get('app_page_title.admin_delivery_methods')
             ],
             [
                 'granted'   => $this->security->isGranted('admin_payment_methods'),
                 'path'      => $this->router->generate('admin_payment_methods'),
-                'isActive'  => $this->currentRoute === 'admin_payment_methods' || $this->currentRoute === 'admin_payment_method_edit',
+                'isActive'  => $currentRoute === 'admin_payment_methods' || $currentRoute === 'admin_payment_method_edit',
                 'title'     => $this->parameterBag->get('app_page_title.admin_payment_methods')
             ],
         ];
